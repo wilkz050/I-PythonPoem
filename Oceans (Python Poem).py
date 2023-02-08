a15841973b23b2ba18c8bb676c9346617226f48d
wilkz050 <90268932+wilkz050@users.noreply.github.com>
2023-02-08 15:52:33 -0500

    Add files via upload

---
+++ Oceans (Python Poem).py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[43]:
+
+
+z = "Oceans🪸"
+y = "by Zoë Wilks"
+
+##ANSI Colors
+OKCYAN = '\033[96m' 
+OKBLUE = '\033[94m'
+
+##print ('\033[1m' + z)
+
+print(OKCYAN + z.upper())
+print(OKBLUE + y.title())
+
+print('\n')
+print('As I lay')
+print('And watch the sun')
+print('Fade away')
+print('Into the edge')
+print('\n')
+print('I feel content')
+print('Floating in water')
+print('Salt cleansing my worries')
+print('\n')
+print('Problems drifting')
+print('Hopes rising')
+print('With lifting tides')
+print('\n')
+print('I feel free')
+print('I feel rest')
+print('Connected to Nature')
+print('\n')
+print('One at last')
+
+
+
+
+# In[ ]:
+
+
+
+
