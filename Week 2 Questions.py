a88aec32b802bd564b1c5dc14c1a051f668e0907
wilkz050 <90268932+wilkz050@users.noreply.github.com>
2023-02-15 15:58:26 -0500

    Add files via upload

---
+++ Week 2 Questions.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# 1) What is the output of the following code and why? 
+
+# var1 = 1
+# var2 = 2
+# var3 = "3"
+
+# print(var1 + var2 + var3)
+
+# a) 6 
+# b) 33 
+# c) 3 + 3 
+# d) Error 
+
+    #MY ANSWER: d, because of string "3"
+
+
+# In[2]:
+
+
+# 2) What is the output of the following code? 
+
+# str = "pynative"
+
+# print (str[1:3])
+
+# a) py
+# b) yn
+# c) pyn
+# d) yna
+
+    #MY ANSWER: b
+
+
+# In[3]:
+
+
+# 3) What is the output of the following code? 
+
+# var= "James Bond"
+# print(var[2::-1])
+
+# a) Jam
+# b) dno
+# c) maJ
+# d) dnoB semaJ
+
+    #MY ANSWER: c
+
+
+# In[4]:
+
+
+# 4) What is the output of the following code? 
+
+# var = "James" * 2  * 3
+# print(var)
+
+# a) JamesJamesJamesJamesJamesJames
+# b) JamesJamesJamesJamesJames
+# c) Error: invalid syntax
+
+    #MY ANSWER: a, six times!
+
+
+# In[12]:
+
+
+# 5) Get rid of the non fruit item: 
+
+# sweets = ['apple', 'bananas', 'cantelopes', 'donut']
+
+    #MY ANSWER: pop function!
+    #sweets.pop()
+    #returns 'donut'
+
+
+# In[6]:
+
+
+# 6) Find "santa" in the variable "house" 
+
+
+# room1=['gabe', 'greg']
+# room2=['michelle','santa','stacey']
+# room3=['adam','sam', 'melissa']
+# house= [room2, room1, room3] 
+
+   #MY ANSWER: 
+    #house[0][1]
+
+
+# In[7]:
+
+
+# 7) A dictionary is the best way to store a sequence of events like a recipe.
+
+# True or false? 
+
+    #MY ANSWER: False!
+
+
+# In[13]:
+
+
+# 8) Remove all dupes 
+# names = ['mary', 'bob', 'jessica', 'pierre', 'bob', 'jennifer', 'chris', 
+#          'christina', 'tina', 'mary', 'tina', 'bob', 'michelle','jennifer']
+
+    #MY ANSWER: 
+    #set(names)
+
+
+# In[9]:
+
+
+# 9) What's the difference between = and == ? 
+    
+    #MY ANSWER: = refers to the assignment operator, while == is whether two values are equal or not.
+
