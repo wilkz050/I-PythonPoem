8741526040c307dd21cc89c96187123b1c5091bf
wilkz050 <90268932+wilkz050@users.noreply.github.com>
2023-02-15 21:35:29 -0500

    Add files via upload

---
+++ Week 3 Questions.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# 1) Iterate through list and print the key 
+
+# fruit_basket = {'apple': 1, 'bananas': 2, 'cantelopes': 3, 'dates': 4, 'figs': 5, 'grapes': 6, 'tangerines': 7, 'watermelon': 8, 'avocado': 9, 'strawberries': 10}
+
+    #MYANSWER: 
+    #for fruit in fruit_basket:
+        #print(fruit_basket[fruit])
+
+
+# In[2]:
+
+
+# 2) Iterate through list and print the value 
+# fruit_basket = {'apple': 1, 'bananas': 2, 'cantelopes': 3, 'dates': 4, 'figs': 5, 'grapes': 6, 'tangerines': 7, 'watermelon': 8, 'avocado': 9, 'strawberries': 10}
+    #MY ANSWER:
+    #for fruit in fruit_basket:
+    #print(fruit)
+
+
+# In[3]:
+
+
+
+
+
+# In[4]:
+
+
+# 4) What is the output of the add() function call and why? 
+
+
+# def add(a, b):
+#     return a+5, b+5
+
+# result = add(3, 2)
+# print(result)
+
+
+# a) 15
+# b) 8
+# c) (8, 7)
+# d) Syntax Error
+
+    #MY ANSWER: (8,7)
+
+
+# In[5]:
+
+
+# 5) Select the statements that are true: 
+
+
+# a) A Python function can return only a single value
+# b) A function can take an unlimited number of arguments.
+# c) A Python function can return multiple values
+# d) Python function doesn’t return anything unless and until you add a return statement
+
+    #MY ANSWER: True: b,c
+
+
+# In[6]:
+
+
+# 6)Python function always returns a value
+
+# True or False and why? 
+
+    #MY ANSWER: Unless it's not defined! (or 'none')
+
+
+# In[7]:
+
+
+# 7) Given the nested if-else structure below, what will be the value of x after code execution completes
+
+# x = 0
+# a = 0
+# b = -5
+# if a > 0:
+#     if b < 0: 
+#         x = x + 5 
+#     elif a > 5:
+#         x = x + 4
+#     else:
+#         x = x + 3
+# else:
+#     x = x + 2
+# print(x)
+
+    #MY ANSWER: 2
+
+
+# In[ ]:
+
+
+# 8) What is the output of the following if statement and why? 
+
+
+# a, b = 12, 5
+# if a + b:
+#     print('True')
+# else:
+#   print('False')
+
+    #MY ANSWER: output is "True" because these are non-zero values.
+
